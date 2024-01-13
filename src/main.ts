37378395f36970b0b65d514d5ae7377b6f5c918b
Mr.Makhmud <m.xudoyberdiev88@gmail.com>
2024-01-14 03:15:17 +0500

    add swagger & confige swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('port');
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth_Lesson')
+    .setDescription('This API forr Lesson')
+    .setVersion('1.0')
+    .addTag('Auth')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
 }
 bootstrap();
